238e15206ef4609377fb0a8d8345ba24c13fc5c8
josiaschweizer <josia.schweizer@gmail.com>
2025-12-05 13:45:55 +0100

    added string-converter project

---
+++ string-converter/src/IStringConverter.java
@@ -0,0 +1,3 @@
+public interface IStringConverter {
+  int convertToInt(String numericString);
+}
---
+++ string-converter/src/NumberConverter.java
@@ -0,0 +1,24 @@
+public class NumberConverter {
+
+  private final IStringConverter stringConverter;
+
+  public NumberConverter(IStringConverter stringConverter) {
+    this.stringConverter = stringConverter;
+  }
+
+  public int roundUp(float value) {
+    return 0; // wie im C#-Code: noch nicht implementiert
+  }
+
+  public int roundDown(float value) {
+    return 0;
+  }
+
+  public int roundToPowerOfTen(float value, int precisionExponent) {
+    return 0;
+  }
+
+  public int roundToPowerOfTen(String numericString, int precisionExponent) {
+    return 0;
+  }
+}
